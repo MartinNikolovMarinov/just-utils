d41cc3af4ad4a186bb6d3a801d36e7c1d3751648
martin_marinov <martin_n_marinov@mail.bg>
2018-11-26 13:46:20 +0200

    cloneObject update

--- prototype_extentions/objectExtension.js
+++ prototype_extentions/objectExtension.js
@@ -18,32 +18,29 @@ function sortObjectByKeys(obj, sortFn) {
   return ret
 }
 
-function cloneObj(obj) {
-  let clonedObj = {}
-  for (let key in obj) {
-    if (obj.hasOwnProperty(key)) clonedObj[key] = obj[key]
+function cloneObject(obj) {
+  if (['undefined', 'boolean', 'number', 'string', 'symbol'].indexOf(typeof obj) > 0) {
+    return obj;
   }
 
-  return clonedObj
+  const clone = {};
+  _cloneObject(obj, clone);
+  return clone;
 }
 
-function extend(child, parent) {
-  let hasProp = {}.hasOwnProperty
-  for (let key in parent) {
-    if (hasProp.call(parent, key)) child[key] = parent[key]
+const _cloneObject = (obj, clone) => {
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      const value = obj[key];
+      if (Array.isArray(value)) {
+        clone[key] = [];
+        _cloneObject(value, clone[key]);
+      } else if (value !== null && typeof value === 'object') {
+        clone[key] = {};
+        _cloneObject(value, clone[key]);
+      } else {
+        clone[key] = value;
+      }
+    }
   }
-
-  function ctor() {
-    this.constructor = child
-  }
-
-  ctor.prototype = parent.prototype
-  child.prototype = new ctor()
-  child.__super__ = parent.prototype
-  return child
-}
-
-Object.prototype.inherit = function (parent) {
-  this.prototype = Object.create(parent.prototype)
-  this.prototype.constructor = this
 }
